da3b29b0f4eb7a9b0e5166293061d63b565e5fa0
novy213 <j.solarek@wp.pl>
2023-10-19 08:17:54 +0200

    add visit functionality

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -3,7 +3,9 @@
 namespace app\controllers;
 
 use app\models\User;
+use app\models\Visit;
 use Yii;
+use yii\base\ViewEvent;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
@@ -97,5 +99,38 @@ class SiteController extends \app\components\Controller
             ];
         }
     }
-
+    public function actionAddvisit(){
+        $post = $this->getJsonInput();
+        $user = Yii::$app->user->identity;
+        //date', 'barber_id', 'price', 'type_id', 'time', 'user_id'
+        $visit = new Visit();
+        if(isset($post->date)){
+            $visit->date = $post->date;
+        }
+        if(isset($post->barber_id)){
+            $visit->barber_id = $post->barber_id;
+        }
+        if(isset($post->price)){
+            $visit->price = $post->price;
+        }
+        if(isset($post->type_id)){
+            $visit->type_id = $post->type_id;
+        }
+        if(isset($post->time)){
+            $visit->time = $post->time;
+        }
+        $visit->user_id = $user->id;
+        if($visit->validate()){
+            $visit->save();
+            return [
+                'error' => FALSE,
+                'message' => NULL,
+            ];
+        } else {
+            return [
+                'error' => true,
+                'message_user' => $visit->getErrorSummary(false),
+            ];
+        }
+    }
 }
