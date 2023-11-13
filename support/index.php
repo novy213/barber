a7ce22fc27822c4a78ef130e51e5886172c0c2d7
novy213 <j.solarek@wp.pl>
2023-11-13 13:25:52 +0100

    add terms and condition, support and privacy policy

---
+++ support/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Usuwanie Konta</title>
+</head>
+<body>
+
+<h2>Instrukcje Usuwania Konta</h2>
+
+<ol>
+    <li>Zaloguj się do swojego konta.</li>
+    <li>Przejdź do ustawień konta.</li>
+    <li>Znajdź opcję "Usuń Konto" i kliknij na nią.</li>
+</ol>
+
+<p>Po naciśnięciu przycisku "Usuń Konto", wszystkie Twoje dane zostaną natychmiast usunięte z naszej bazy danych i nie będą nigdzie przechowywane.</p>
+
+<p>W razie pytań prosimy o kontakt na adres e-mail: <a href="mailto:philiprudzinsky@gmail.com">philiprudzinsky@gmail.com</a>.</p>
+
+</body>
+</html>
