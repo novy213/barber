a97c976ef9ab8c9b8fc26a3d2be26b619056bd05
novy213 <j.solarek@wp.pl>
2023-11-27 18:28:08 +0100

    fixed bugs

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -446,7 +446,7 @@ class SiteController extends \app\components\Controller
         ];
     }
     public function actionGetprices(){
-        $types = Type::find()->andWhere(['<>', 'id', 8])->all();
+        $types = Type::find()->andWhere(['<>', 'type', 'dayoff'])->all();
         $additional = AdditionalServices::find()->all();
         return [
             'error' => FALSE,
