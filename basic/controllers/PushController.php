dd899f5e6626bf57408a8220b624eb0cb704c06d
novy213 <j.solarek@wp.pl>
2023-12-09 11:21:37 +0100

    test push notification commit

--- basic/controllers/PushController.php
+++ basic/controllers/PushController.php
@@ -11,20 +11,51 @@ use sngrl\PhpFirebaseCloudMessaging\Notification;
 class PushController extends Controller
 {
     public function actionPushnoti(){
-        $server_key = 'AAAAgX5aCbI:APA91bFWRppLu34vfmvXfcSN4IZH3oiXJF92hneHyTf7TJiNbguEjYjAUVVFX0TmZh5IpA_cmA-5pA8lNe7CE-S2cwUAixO8kvJhhKIziKWa2VNqMScmHtgS8XM_JwJN31A8wo42BrLS';
-        $client = new Client();
-        $client->setApiKey($server_key);
+        $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+        $args = func_get_args();
+        $response = ["status" => 0, "message" => "Notification couldn't be sent"];
 
-        $message = new Message();
-        $message->setPriority('high');
-        $message->addRecipient(new Device('xOZCohBL_nVysa6XnoBg28'));
-        $message
-            ->setNotification(new Notification('Ala ma kota tytul', 'w ogolnym rozrachunku kot ma kurwa ale'))
-            ->setData(['key' => 'KOT'])
-        ;
+        $title = "Hey test notification";
+        $body = "";
+        $apiKey = "AAAAgX5aCbI:APA91bFWRppLu34vfmvXfcSN4IZH3oiXJF92hneHyTf7TJiNbguEjYjAUVVFX0TmZh5IpA_cmA-5pA8lNe7CE-S2cwUAixO8kvJhhKIziKWa2VNqMScmHtgS8XM_JwJN31A8wo42BrLS";//Server key under the Project settings
+        $tokenArr = ["ExponentPushToken[xOZCohBL_nVysa6XnoBg28]"];
+        $refId = 123;
+        $msgNotification = [
+            "title" => $title,
+            "body" => $body
+        ];
+        $extraNotificationData = [
+            "refId" => $refId,
+            "title" => $title
+        ];
 
-        $response = $client->send($message);
-        var_dump($response->getStatusCode());
-        var_dump($response->getBody()->getContents());
+        $fcmNotification = [
+            "registration_ids" => $tokenArr,
+            "notification" => $msgNotification,
+            "data" => $extraNotificationData
+        ];
+        $headers = [
+            "Authorization: key=" . $apiKey,
+            "Content-Type: application/json"
+        ];
+
+        $encodedData = json_encode($fcmNotification);
+        // echo "<pre>";print_r($fcmNotification);exit;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$fcmUrl);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+        $result = curl_exec($ch);
+        if ($result === FALSE) {
+            die("Curl failed: " . curl_error($ch));
+        }
+        print_r($result);exit;
+        curl_close($ch);
+        $response = ["status" => 1, "message" => "Notification sent to users", "payload" => $result];
+
+        return $response;
     }
 }
