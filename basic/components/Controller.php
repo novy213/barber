766ce9fcbc87837c06db3ab5f66de73c3c20cdbd
novy213 <j.solarek@wp.pl>
2024-02-26 16:51:12 +0100

    add reset password functionality

--- basic/components/Controller.php
+++ basic/components/Controller.php
@@ -21,7 +21,7 @@ class Controller extends \yii\web\Controller
         $behaviors =  parent::behaviors();
         $behaviors['userTokenAuth'] = [
             'class' => HttpBearerAuth::className(),
-            'except' => ['login', 'register', 'sendnoti', 'smsforpassword', 'test'],
+            'except' => ['login', 'register', 'sendnoti', 'smsforpassword', 'test', 'resetpassword'],
         ];
         return $behaviors;
     }
